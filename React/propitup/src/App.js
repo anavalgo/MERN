ebafeebb02ce43be30b62acac627f7eecf22b6fe
anavalgo <88244757+anavalgo@users.noreply.github.com>
2022-04-16 21:57:10 -0500

    Update App.js

--- React/propitup/src/App.js
+++ React/propitup/src/App.js
@@ -3,19 +3,36 @@ import './App.css';
 import PersonCard from './components/PersonCard';
 
 
+const peopleArr = [
+  {
+    firstName: "Ana", 
+    lastName: "Gomez",
+    age: 28, 
+    hair: "brown",
+  },
+  {
+    firstName: "Ben", 
+    lastName: "Harper",
+    age: 22, 
+    hair: "blonde",
+  },
+  {
+    firstName: "George", 
+    lastName: "Blake",
+    age: 44, 
+    hair: "black",
+  }
+];
+
 function App() {
   return (
   <div className="App">
-
-      <PersonCard lastName={"Gomez"} firstName={"Ana"} age={28}  hair={"dark brown"} />
-      
-      <PersonCard lastName={"Harper"} firstName={"Ben"} age={22} hair={"black"} />
-      
-      <PersonCard lastName={"Blake"} firstName={"George"} age={22} hair={"black"} />
-
-      </div>
-      );
-      }
+    {peopleArr.map((personObj, index) => (
+      <PersonCard key={index} firstName={personObj.firstName} lastName={personObj.lastName} age={personObj.age} hair={personObj.hair} />
+    ))}
+  </div>
+  );
+    }
 
 export default App;
 
